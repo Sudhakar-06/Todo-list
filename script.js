15c7e34bcc473dfe8603df440125e6ce3641bf3b
Sudhakar-Dev <sudhakarrali516@gmail.com>
2026-09-18 17:50:06 +0530

    Refactor task management functions and event listeners

--- script.js
+++ script.js
@@ -1,55 +1,57 @@
-//grab elements from the page
+// Grab elements from the page
 const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
 
-//array to store tasks
-let tasks[];
+//Arrays to store tasks
+let tasks = [];
 
-//function to render the list on the page
+// function to render the list on the page
 function renderTasks() {
-	taskList.innerHTML = "";
-	for (let i=0; i<tasks.length; i++) {
-		let li = document.createElement("li");
-		if (tasks[i].completed)  {
-			li.classList.add("completed");
-		}
+  taskList.innerHTML = "";  // clear the list first
 
-		let span = document.createElement("span");
-		span.textContent = tasks[i].text;
-		span.addEventListener("click" , function() {
-			tasks[i].completed = !tasks[i].completed;
-			renderTasks();
-		});
+  for (let i=0; i<tasks.length; i++) {
+    let li = document.createElement("li");
+    if(tasks[i].completed) {
+      li.classList.add("completed");
+    }
+    let span = document.createElement("span");
+    span.textContent = tasks[i].text;
+    span.addEventListener("click", function() {
+      tasks[i].completed = !tasks[i].completed;
+      renderTasks();
+    });
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.addEventListener("click", function() {
+      tasks.splice(i, 1);
+      renderTasks();
+    });
 
-		let deleteBtn = document.createElement("button");
-		deleteBtn.textContent = "Delete";
-		deleteBtn.classList.add("delete-btn");
-		deleteBtn.addEventListener("click" , function(){
-			tasks.splice(i,1):
-			renderTasks();
-		});
-		li.appendChild(span);
-		li.appendChild(deleteBtn);
-		taskList.appendChild(li);
-	}
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+  }
 }
 
-//function to add a new task
+// function to add new task
 function addTask() {
-	let taskText = taskInput.value.trim();
-	if (taskText === "") {
-		return;
-	}
-	tasks.push({text: taskText; completed: false});
-	taskInput.value = "";
-	renderTasks();
+  let taskText = taskInput.value.trim();
+  if (taskText === "") {
+    return;
+  }
+
+  tasks.push({text: taskText , completed: false});
+  taskInput.value = "";
+  renderTasks();
 }
 
-// event listeners
-addBtn.addEventListener("click" , addTask);
-taskInput.addEventListener("keypress" , function(event)) {
-	if (event.key === "Enter") {
-		addTask();
-	}
-});
+//event listeners
+addBtn.addEventListener("click", addTask);
+
+taskInput.addEventListener("keypress", function(event) {
+  if(event.key === "Enter") {
+    addTask();
+  }
+})
